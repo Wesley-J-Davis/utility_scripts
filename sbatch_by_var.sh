f07df939732e092a8e65671cacc81e2b80d54e9a
Wesley Davis <60855573+Wesley-J-Davis@users.noreply.github.com>
2025-05-05 22:23:13 -0400

    Update sbatch_by_var.sh

--- sbatch_by_var.sh
+++ sbatch_by_var.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
+while read -r item; do
+    echo "Executing script for: $item"
+    ./my_script.sh "$item"
+done < list.txt
 # Define a list of variables
-variables=("param1" "param2" "param3" "param4")
 
-# Iterate through each variable and submit a job
-for var in "${variables[@]}"; do
-    echo "Submitting job with variable: $var"
-    
-    sbatch --export=MYVAR=$var my_slurm_script.sh
-done
-
-echo "All jobs submitted!"
+#!/bin/bash
+#var=$1
+#sbatch --export=MYVAR=$var my_slurm_script.sh
